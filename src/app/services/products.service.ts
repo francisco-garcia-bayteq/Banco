2c96fb476dbecb1fc91eb42465a4e48e044cb29f
Francisco García M. <francisco.garcia@bayteq.com>
2025-07-15 00:23:56 -0500

    feature: Inicial commit, services, interfaces

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { Product } from "../utils/models/product.interface";
+import { environment } from "../environment/environment";
+import { AplicationService } from "../utils/base/aplication.service";
+import { HttpClient } from "@angular/common/http";
+import { ApiResponse } from "../utils/models/api.interface";
+import { Observable } from "rxjs";
+
+@Injectable({
+	providedIn: 'root'
+})
+export class ProductsService extends AplicationService {
+
+	constructor(private readonly _http: HttpClient) {
+		super();
+	}
+
+	getProducts(): Observable<ApiResponse<Product[]>> {
+		return this._http.get<ApiResponse<Product[]>>(`${this.getApiBaseUrl()}/bp/products`, { headers: this.getHeaders() });
+	}
+
+    createProduct(product: Product): Observable<ApiResponse<Product>> {
+        return this._http.post<ApiResponse<Product>>(`${this.getApiBaseUrl()}/bp/products`, product, { headers: this.getHeaders() });
+    }
+
+    updateProduct(product: Product): Observable<ApiResponse<Product>> {
+        return this._http.put<ApiResponse<Product>>(`${this.getApiBaseUrl()}/bp/products/${product.id}`, product, { headers: this.getHeaders() });
+    }
+    
+}
